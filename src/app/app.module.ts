ddf796db1e05bf36801ae0ff4fe15ebdfab0fe4e
FI4 OPEN SOURCE <rpcolom@gmail.com>
2021-07-07 08:38:21 +0200

    toqueteos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
+import { BookCollectionComponent } from './book-collection/book-collection.component';
+//import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { booksReducer } from './state/books.reducer';
+import { collectionReducer } from './state/collection.reducer';
+import { StoreModule } from '@ngrx/store';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    StoreModule.forRoot({ books: booksReducer, collection: collectionReducer }),
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, BookListComponent, BookCollectionComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
